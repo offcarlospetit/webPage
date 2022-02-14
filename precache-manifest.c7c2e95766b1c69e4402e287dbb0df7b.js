500c1de53ee8b46325758dd4930100a1c7b8bdd4
Carlos Petit <ca.alberto.p@gmail.com>
2022-02-14 08:57:12 -0300

    Updates

---
+++ precache-manifest.c7c2e95766b1c69e4402e287dbb0df7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85b73eadd592be89aa43",
+    "url": "/webPage/static/css/main.37b385a9.chunk.css"
+  },
+  {
+    "revision": "85b73eadd592be89aa43",
+    "url": "/webPage/static/js/main.85b73ead.chunk.js"
+  },
+  {
+    "revision": "bcea34df45f725e2559c",
+    "url": "/webPage/static/js/runtime~main.bcea34df.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/webPage/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7700ed577ca5b430e5f4d67a0f37a790",
+    "url": "/webPage/index.html"
+  }
+];
